c428c86aa3bf9400d6fc863ca63ea45e32f1c19c
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2019-05-28 18:20:49 +0200

    Metadata to Elasticsearch import

---
+++ narcisweb/bin/metadata2elastic.py
@@ -0,0 +1,47 @@
+#!/usr/local/bin/python
+from __future__ import print_function, absolute_import
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+from pymongo import MongoClient
+from os import walk
+import simplejson
+import json
+import logging
+from elasticsearch import Elasticsearch
+logging.basicConfig(filename='logs/importmetadata.log',format='%(asctime)s %(levelname)s  %(message)s', datefmt='%Y-%m-%d %H:%M:%S',level=logging.DEBUG)
+
+path = "/exchange"
+metadatapath = path
+print("Importing metadata from %s" % metadatapath)
+logging.info("Importing metadata from %s" % metadatapath)
+
+client = MongoClient('mongodb://mongonarcis:27017')
+datasetdb = client.get_database('narcis')
+col = datasetdb.data
+
+f = []
+for (dirpath, dirnames, filenames) in walk("%s" % metadatapath):
+    f.extend(filenames)
+
+for filename in f:
+    filepath = "%s/%s" % (path, filename)
+    file = open(filepath, 'r')
+    print(filepath)
+    for lastline in file:
+        metadata = json.loads(lastline)
+        url = "https://www.narcis.nl/dataset/RecordID/%s" % metadata['id']
+        info = {}
+        info[url] = metadata
+
+        try:
+            es.create(index=newindex, doc_type=newcol, body=info[url], id=info[url]['id'])
+        except:
+            es.delete(index=newindex, doc_type=newcol, id=info[url]['urlid'])
+            es.create(index=newindex, doc_type=newcol, body=info[url], id=info[url]['id'])
+            logging.error("Error in inserting %s into 'dataset' database" % (path + "/" + filename))
+
+print("Metadata imported")
+logging.info("Metadata imported")
+
