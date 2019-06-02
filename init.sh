21d4262bf04b8c43be59a9b178dbed72b83c4dac
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2019-06-03 01:45:05 +0200

    Init script to update all data

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Upload NARCIS data to MongoDB
+docker exec augmenting-narcis_web_1 '/narcis/bin/importmetadata.py'
+# Then upload the same data to elastic
+docker exec augmenting-narcis_web_1 '/narcis/bin/metadata2elastic.py'
