0b3faf82e5976c0f7881a8d0c81fa153ce4ce637
Tijmen van Etten <tijmenvanetten@wcw-stud-145-109-35-117.wireless.uva.nl>
2019-06-12 19:03:05 +0200

    Added script that imports grid data into elasticsearch database

---
+++ importgrid.py
@@ -0,0 +1,12 @@
+from elasticsearch import helpers, Elasticsearch
+import csv
+
+es = Elasticsearch(
+['elasticsnarcis'],
+http_auth=('elastic', 'changeme'),
+port=9200
+)
+
+with open('/exchange/grid/grid.csv') as grid:
+    reader = csv.DictReader(grid)
+    helpers.bulk(es, reader, index='grid', doc_type='metadata')
