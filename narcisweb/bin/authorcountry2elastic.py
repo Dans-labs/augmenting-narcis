cf8d34b5d8e0e3c38dbabb5ad0c22d867b47e3a6
Kaj Meijer <k.d.meijer17@gmail.com>
2019-06-22 21:51:33 +0200

    created a script that creates the author/country index for elasticsearch and updated the doiboost2elastic

--- narcisweb/bin/authorcountry2elastic.py
+++ narcisweb/bin/authorcountry2elastic.py
@@ -19,7 +19,7 @@ es = Elasticsearch(
     port=9200
     )
 
-path = "/exchange/DOIboost2017"
+path = "/exchange/Authors"
 metadatapath = path
 
 print("Importing metadata from %s" % metadatapath)
@@ -28,17 +28,24 @@ f = []
 for (dirpath, dirnames, filenames) in walk("%s" % metadatapath):
     f.extend(filenames)
 
+print("Files to be uploaded:", len(f))
+
+i = 0
 for filename in f:
+
+    i += 1
     filepath = "%s/%s" % (path, filename)
     file = open(filepath, 'r')
 
     print(filepath)
 
     for lastline in file:
-        metadata = ast.literal_eval(json.loads(lastline))
+        metadata = json.loads(lastline)
         try:
-            helpers.bulk(es, metadata, index='doiboost2017', doc_type='metadata')
+            helpers.bulk(es, metadata, index='authorcountry', doc_type='metadata')
         except:
-            continue
+            print("Error.")
+
+    print(i, "files out of ", len(f), "uploaded.")
 
 print("Metadata imported")
